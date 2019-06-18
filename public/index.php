49dd012ab3910c8c3b010cafae41bda596c0835a
李明尚 <limingshang@yunlibeauty.com>
2019-06-18 16:07:44 +0800

    修改

--- public/index.php
+++ public/index.php
@@ -9,7 +9,8 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 // [ 应用入口文件 ]
-
+header('Access-Control-Allow-Origin:http://h5.wquant.develop');
+header('Access-Control-Allow-Origin:http://wk.yngw518.com');
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
 // 加载框架引导文件
